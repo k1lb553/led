180ae99cd6f408364d529b388ef20a8838ed2dda
RBence07 <82832084+RBence07@users.noreply.github.com>
2022-01-29 21:50:34 +0100

    Update code.js

--- code.js
+++ code.js
@@ -1,130 +1,102 @@
-body {
-    padding: 0px;
-    margin-top: 20px;
-    background-color: #121212;
-    color: white;
-    font-family: Verdana;
-}
-
-h2 {
-    text-align: center ;
-    font-size: 24px;
-}
-
-p {
-    font-size: 16px;
-    text-align: left;
-    margin: 0px 0px 2px 18px;
-    color: whitesmoke;
-}
-
-.main_div{
-    margin: 10px 10px 10px 10px;
-    display:inline-block;
-    padding: 20px;
-    border-radius: 10px;
-    background-color: #2c2c2c;
-}
-
-.brightness_div{
-    margin-bottom: 10px;
-    box-sizing: border-box;
-    width: 300px;
-    height: 40px;
-    border-radius: 20px;
-    border: 1px solid white;
-    background: linear-gradient(to right, rgb(0, 0, 0) 0%, rgb(255, 255, 255) 100%);
-}
-
-.kelvin_div{
-    box-sizing: border-box;
-    width: 300px;
-    height: 40px;
-    border-radius: 20px;
-    border: 1px solid white;
-    background: linear-gradient(to right, rgb(255, 148, 42) 0%, rgb(255, 189, 126) 12.5%, rgb(255, 216, 181) 25%, rgb(255, 235, 222) 37.5%, rgb(255, 248, 255) 50%, rgb(230, 235, 255) 62.5%, rgb(214, 226, 255) 75%, rgb(204, 220, 255) 87.5%);
-}
-
-.slider{
-    -webkit-appearance: none;
-    margin: 0;
-    width: 280px;
-    height: 40px; opacity: 1;
-    background: none;
-}
-
-.slider::-webkit-slider-thumb {
-    -webkit-appearance: none;
-    background: white;
-    width: 16px;
-    height: 16px;
-    cursor: pointer;
-    border-radius: 20px;
-    border: 2.5px solid black;
-}
-
-@media only screen and (max-width: 1000px) {
-    body {
-        padding: 0px;
-        margin-top: 46px;
-        background-color: #121212;
-        color: white;
-        font-family: Verdana;
+var data = ["255,255,255","255","255","255,255,255","255","255","255,255,255","255","255"];
+var size=300;
+var radius=8;
+if(window.innerWidth < 1000) {
+        size=690;
+        radius=18.4;
     }
-    
-    h2 {
-        font-size: 55.2px;
-        text-align: center ;
+else{
+        size=300;
+        radius=8;
     }
-    
-    p {
-        font-size: 36.8px;
-        text-align: left;
-        margin: 0px 0px 4.6px 41.4px;
-        color: whitesmoke;
-    }
-    
-    .main_div{
-        margin: 23px 23px 23px 23px;
-        display:inline-block;
-        padding: 46px;
-        border-radius: 23px;
-        background-color: #2c2c2c;
-    }
-    
-    .brightness_div{
-        margin-bottom: 23px;
-        box-sizing: border-box;
-        width: 690px;
-        height: 92px;
-        border-radius: 46px;
-        border: 2.3px solid white;
-        background: linear-gradient(to right, rgb(0, 0, 0) 0%, rgb(255, 255, 255) 100%);
-    }
-    
-    .kelvin_div{
-        box-sizing: border-box;
-        width: 690px;
-        height: 92px;
-        border-radius: 46px;
-        border: 2.3px solid white;
-        background: linear-gradient(to right, rgb(255, 148, 42) 0%, rgb(255, 189, 126) 12.5%, rgb(255, 216, 181) 25%, rgb(255, 235, 222) 37.5%, rgb(255, 248, 255) 50%, rgb(230, 235, 255) 62.5%, rgb(214, 226, 255) 75%, rgb(204, 220, 255) 87.5%);
-    }
-    
-    .slider{
-        -webkit-appearance: none;
-        margin: 0;
-        width: 644px;
-        height: 46px; opacity: 1;
-        background: none;
-    }
-    
-    .slider::-webkit-slider-thumb {
-        -webkit-appearance: none;
-        background: white;
-        width: 36.8px;
-        height: 36.8px;
-        cursor: pointer;
-        border-radius: 46px;
-        border: 5.75px solid black;
-    }}
+
+window.location.href="index.html#"+data
+var colorPicker = new iro.ColorPicker('#picker1', {
+    width: size,
+    handleRadius: radius,
+    borderWidth: 1,
+    borderColor: "#fff",
+    layout: [
+    { 
+        component: iro.ui.Wheel,
+        options: {}
+    },
+    ]
+});
+colorPicker.on(['color:init', 'input:end'], function(color) {
+    data[0] = color.red+","+color.green+","+color.blue;
+    window.location.href="index.html#"+data;
+});
+
+
+var colorPicker = new iro.ColorPicker('#picker2', {
+    width: size,
+    handleRadius: radius,
+    borderWidth: 1,
+    borderColor: "#fff",
+    layout: [
+    { 
+        component: iro.ui.Wheel,
+        options: {}
+    },
+    ]
+});
+
+colorPicker.on(['color:init', 'input:end'], function(color) {
+    data[3] = color.red+","+color.green+","+color.blue;
+    window.location.href="index.html#"+data;
+    });
+
+var colorPicker = new iro.ColorPicker('#picker3', {
+    width: size,
+    handleRadius: radius,
+    borderWidth: 1,
+    borderColor: "#fff",
+    layout: [
+    { 
+        component: iro.ui.Wheel,
+        options: {}
+    },
+    ]
+});
+
+colorPicker.on(['color:init', 'input:end'], function(color) {
+    data[6] = color.red+","+color.green+","+color.blue;
+    window.location.href="index.html#"+data;
+    });
+
+function brightness_1(){
+    var value = document.getElementById("brightness_1").value;
+    data[1] = value;
+    window.location.href="index.html#"+data;
+}
+
+function kelvin_1(){
+    var value = document.getElementById("kelvin_1").value;
+    data[2] = value;
+    window.location.href="index.html#"+data;
+}
+
+function brightness_2(){
+    var value = document.getElementById("brightness_2").value;
+    data[4] = value;
+    window.location.href="index.html#"+data;
+}
+
+function kelvin_2(){
+    var value = document.getElementById("kelvin_2").value;
+    data[5] = value;
+    window.location.href="index.html#"+data;
+}
+
+function brightness_3(){
+    var value = document.getElementById("brightness_3").value;
+    data[7] = value;
+    window.location.href="index.html#"+data;
+}
+
+function kelvin_3(){
+    var value = document.getElementById("kelvin_3").value;
+    data[8] = value;
+    window.location.href="index.html#"+data;
+}
