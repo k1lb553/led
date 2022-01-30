d4ee4f38801050267d9b058b059046507283108c
RBence07 <82832084+RBence07@users.noreply.github.com>
2022-01-30 18:47:03 +0100

    Update code.js

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-var data = ["","255,255,255","255","255","255,255,255","255","255","255,255,255","255","255","0","255","255","*"];
+
 var size=300;
 var radius=8;
 if(window.innerWidth < 1000) {
@@ -18,7 +18,6 @@ if (isMobile) {
     element.innerHTML = "You are using Desktop";
 }
     
-window.location.href=data
 var colorPicker = new iro.ColorPicker('#picker1', {
     width: size,
     handleRadius: radius,
@@ -33,7 +32,6 @@ var colorPicker = new iro.ColorPicker('#picker1', {
 });
 colorPicker.on(['color:init', 'input:end'], function(color) {
     data[1] = color.red+","+color.green+","+color.blue;
-    window.location.href=data
 });
 
 
@@ -52,7 +50,6 @@ var colorPicker = new iro.ColorPicker('#picker2', {
 
 colorPicker.on(['color:init', 'input:end'], function(color) {
     data[4] = color.red+","+color.green+","+color.blue;
-    window.location.href=data
     });
 
 var colorPicker = new iro.ColorPicker('#picker3', {
@@ -70,59 +67,49 @@ var colorPicker = new iro.ColorPicker('#picker3', {
 
 colorPicker.on(['color:init', 'input:end'], function(color) {
     data[7] = color.red+","+color.green+","+color.blue;
-    window.location.href=data
     });
 
 function brightness_1(){
     var value = document.getElementById("brightness_1").value;
     data[2] = value;
-    window.location.href=data
 }
 
 function kelvin_1(){
     var value = document.getElementById("kelvin_1").value;
     data[3] = value;
-    window.location.href=data
 }
 
 function brightness_2(){
     var value = document.getElementById("brightness_2").value;
     data[5] = value;
-    window.location.href=data
 }
 
 function kelvin_2(){
     var value = document.getElementById("kelvin_2").value;
     data[6] = value;
-    window.location.href=data
 }
 
 function brightness_3(){
     var value = document.getElementById("brightness_3").value;
     data[8] = value;
-    window.location.href=data
 }
 
 function kelvin_3(){
     var value = document.getElementById("kelvin_3").value;
     data[9] = value;
-    window.location.href=data
 }
 
 function curtain_1(){
     var value = document.getElementById("curtain_1").value;
     data[10] = value;
-    window.location.href=data
 }
 
 function brightness_4(){
     var value = document.getElementById("brightness_4").value;
     data[11] = value;
-    window.location.href=data
 }
 
 function brightness_5(){
     var value = document.getElementById("brightness_5").value;
     data[12] = value;
-    window.location.href=data
 }
